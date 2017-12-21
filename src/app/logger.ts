78e92545aa3ed8949c84a4b90e8ecadffc57187f
Aliaksandr Kazhamiaka <aliaksandr.kazhamiaka@gmail.com>
2017-12-21 23:58:01 +0300

    Update logger

--- src/app/logger.ts
+++ src/app/logger.ts
@@ -1,5 +1,48 @@
-export class Logger{
-    static write(value: any): void{
-        console.log(value);
+export class Logger {
+    static write(text: string): void {
+        console.log(text);
+    }
+
+    static newTaskAppeared(
+        taskId: string
+    ): void {
+        console.log(`Заявка ${taskId} появилась в системе.`);
+    }
+
+    static startProcessingTask(
+        taskId: string,
+        phaseId: number,
+        channelId: number
+    ): void {
+        console.log(`Заявка ${taskId} начала обрабатываться в фазе ${phaseId} каналом ${channelId}.`);
+    }
+
+    static OnCompletedTask(
+        phaseId: number,
+        channelId: number,
+        taskId: string,
+        time: number
+    ): void {
+        console.log(`Заявка ${taskId} обработана фазой ${phaseId} каналом ${channelId} за время ${time}ms.`);
+    }
+
+    static resultTimeOfProcessingTask(
+        time: number
+    ): void {
+        console.log(`Время обработки заявки: ${time}.`);
+    }
+
+    static rejectTask(
+        taskId: string,
+        phaseId: number
+    ): void {
+        console.log(`Заявка ${taskId} отклонена фазой ${phaseId}.`);
+    }
+
+    static successfullyCompletedTask(
+        taskId: string,
+        phaseId: number
+    ): void {
+        console.log(`Заявка ${taskId} успешно обработана системой.`);
     }
 }
